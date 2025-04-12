edcd939f382af4cb11e61f686c53c7f1d55816fa
Madison Davis <madisond2302@gmail.com>
2025-04-12 15:03:22 -0400

    tiny config comment change

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@
 
 # +++++++++++++ Variables +++++++++++++ #
 LB_HOSTS            = ["127.0.0.1"]         # all machine hosts that the load-balancers could be found on
-LB_PID_RANGE        = [0, 100]              # the range of PIDs that the load-balancers could be found on (for V0, we're only making ONE LB)
+LB_PID_RANGE        = [0, 100]              # the range of PIDs that our single load-balancer can be insantiated with (V1: we'll have 1+ LBs)
 LB_BASE_PORT        = 6000                  # the base port for load-balancers (port = base_port + PID)
 SERVER_BASE_PORT    = 5000                  # the base port for servers (port = base_port + PID)
 SERVER_REGIONS      = [0, 1, 2]             # the current geographic regions our app can serve, represented with number IDs
